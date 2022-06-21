0c1ff8a1badd93a8a072b02ba0dd69e95b94a1c0
yuho0104 <tounaiyuho0104@icloud.com>
2022-06-21 14:57:23 +0900

    text96

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,19 +1,18 @@
-<html>
-<head>
-   <title>Hello/Index</title>
-   <style>
-   body {font-size:16pt; color:#999; }
-   h1 { font-size:50pt; text-align:right; color:#f6f6f6;
-       margin:-20px 0px -30px 0px; letter-spacing:-4pt; }
-   </style>
-</head>
-<body>
-    <h1>Blade/Index</h1>
-    <p>&#064;foreachディレクティブの例</p>
-    <ol>
-    @foreach($data as $item)
-    <li>{{$item}}
-    @endforeach
-    </ol>
- </body>
-</html>
+@extends('layouts.helloapp')
+
+@section('title', 'Index')
+
+@section('menubar')
+   @parent
+   インデックスページ
+@endsection
+@section('content')
+   <p>ここが本文のコンテンツです。</p>
+   <ul>
+   @each('components.item', $data, 'item')
+   </ul>
+@endsection
+
+@section('footer')
+copyright 2020 tuyano.
+@endsection
